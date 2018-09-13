d7da4c757177fa1b8cd3126adf0317a10a6128b4
Hounsvad <github@hounsvad.dk>
2018-09-13 19:05:22 +0200

    Completion of C5

--- src/oopmodules/AssignmentsModule4.java
+++ src/oopmodules/AssignmentsModule4.java
@@ -179,7 +179,23 @@ public class AssignmentsModule4 {
     }
 
     void task_c_5() {
-
+        boolean found = false;
+        int lastPrime = 0;
+//        long time = System.nanoTime();
+        for (int i = 3; i < 1000000; i += 2) {
+            found = false;
+            for (int j = 2; j < Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                lastPrime = i;
+            }
+        }
+        System.out.println(lastPrime);
+        //System.out.println(((double) (System.nanoTime() - time)) / 1000000000 + " Seconds");
     }
 
     void task_c_6() {
