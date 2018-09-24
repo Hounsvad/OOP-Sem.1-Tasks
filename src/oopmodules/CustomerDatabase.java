28d8710ae2f799596db61e9a3ddea992c6e095b9
Hounsvad <github@hounsvad.dk>
2018-09-24 23:32:51 +0200

    Done with m7

--- src/oopmodules/CustomerDatabase.java
+++ src/oopmodules/CustomerDatabase.java
@@ -13,9 +13,17 @@ public class CustomerDatabase {
 
     Customer[] customers = new Customer[200];
 
+    /**
+     * Empty constructer for no apparant reason
+     */
     public CustomerDatabase() {
     }
-
+    
+    /**
+     * Adds a customer if possible
+     * @param _customer A customer object of type Customer
+     * @return true if an error occured else false
+     */
     public boolean addCustomer(Customer _customer) {
         for (int i = 0; i < customers.length; i++) {
             if (customers[i] == null) {
@@ -26,7 +34,11 @@ public class CustomerDatabase {
         System.out.println("There were an issue adding the customer to the database");
         return true;
     }
-
+    /**
+     * Removes a customer if possible
+     * @param _id An id of an existing customer in the database
+     * @return true if an error occured else false
+     */
     public boolean removeCustomer(int _id) {
         for (int i = 0; i < customers.length; i++) {
             if (customers[i].id == _id) {
@@ -37,11 +49,18 @@ public class CustomerDatabase {
         System.out.println("There were an issue removing the customer from the database");
         return true;
     }
-
+    
+    /**
+     * Returns the database array
+     * @return array of Customer
+     */
     public Customer[] getCustomers() {
         return customers;
     }
 
+    /**
+     * Printes the names of the customers in the database
+     */
     public void printCustomerNames() {
         for (Customer customer : customers) {
             if (customer != null) {
