272230ab7e06122be267de848f11396f718e405a
Frederik Hounsvad <Frederik@hounsvad.dk>
2018-10-02 21:54:55 +0200

    Adding of module 9 and finishing it

---
+++ src/oopmodule9/ProductDatabase.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopmodule9;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Pinnacle F
+ */
+public class ProductDatabase {
+
+    Product[] list;
+    private int initialListLength = -1;
+
+    public ProductDatabase() {
+        //If nothing is specified then make the size 1
+        this(1);
+    }
+
+    public ProductDatabase(int initialDatabaseSize) {
+        this.list = new Product[initialDatabaseSize];
+    }
+
+    public boolean addProduct(Product product) {
+        for (int i = 0; i < this.list.length; i++) {
+            if (this.list[i] == null) {
+                this.list[i] = product;
+                return true;
+            }
+        }
+        this.list = Arrays.copyOf(this.list, this.list.length + 1);
+        this.list[this.list.length] = product;
+        return false;
+    }
+
+    public boolean removeProduct(int itemID) {
+        int foundIndex = -1;
+        for (int i = 0; i < this.list.length; i++) {
+            if (this.list[i].getItemID() == itemID) {
+                foundIndex = i;
+
+            }
+        }
+        if (foundIndex != -1) {
+
+            for (int i = foundIndex; i < this.list.length - 1; i++) {
+                this.list[i] = this.list[i + 1];
+            }
+            
+            this.list = Arrays.copyOf(this.list, this.list.length-1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+
+        return "";
+    }
+}
